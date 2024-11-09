ea00d339ef318a61815336c9e20fea00492f8e8c
Anastasia <thedevil_2@mail.ru>
2024-11-09 19:10:56 +0700

    Добавьте файлы проекта.

---
+++ PR5_Guselnikova_Mod/Helper.cs
@@ -0,0 +1,23 @@
+﻿using PR5_Guselnikova_Mod.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR5_Guselnikova_Mod
+{
+    public class Helper
+    {
+        private static Real_Estate_AgencyEntities _context;
+
+        public static Real_Estate_AgencyEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new Real_Estate_AgencyEntities();
+            }
+            return _context;
+        }
+    }
+}
---
+++ PR5_Guselnikova_Mod/Program.cs
@@ -0,0 +1,53 @@
+﻿using PR5_Guselnikova_Mod.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR5_Guselnikova_Mod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = Helper.GetContext();
+            Console.WriteLine("Создание новой учетной записи");
+
+            Console.Write("Введите почту: ");
+            string employee_email = Console.ReadLine();
+            Employee employee = db.Employee.Where(x => x.Email == employee_email).FirstOrDefault();
+
+            if (employee != null)
+            {
+                Console.Write("Введите логин: ");
+                string login = Console.ReadLine();
+
+                Console.Write("Введите пароль: ");
+                string password = Console.ReadLine();
+
+                Console.WriteLine("Выберите роль:");
+                var roles = db.Roles.ToList();
+                foreach (var role in roles) 
+                {
+                    Console.WriteLine(role.ID_Role + " | " + role.Name);
+                }
+
+                int IDroles = Convert.ToInt32(Console.ReadLine());
+                string hash = HashPasswords.Hash.HashPassword(password);
+
+                Console.Write("Хэшированный пароль пользователя: " + hash);
+                
+
+                Models.Authorization authorization = new Models.Authorization
+                {
+                    Login = login,
+                    Password = hash,
+                    RoleID = IDroles
+                };
+                db.Authorization.Add(authorization);
+                db.SaveChanges();
+            }
+        }
+    }
+}
